cd6cd287daed4bbf4b9aa457204c2ee445401215
Vladimir <vladimir@192.168.1.2>
2013-08-23 15:54:53 +0300

    Initial Commit

---
+++ RSSReader/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RSSReader
+//
+//  Created by Vladimir on 23.08.13.
+//  Copyright (c) 2013 Vladimir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
